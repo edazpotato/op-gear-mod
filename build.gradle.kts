65d36a0706777c849c897d3ee0f04bd8c64d9235
Edazpotato <edazpotato@gmail.com>
2021-08-16 18:01:57 +1200

    Setup

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    id("fabric-loom")
+    val kotlinVersion: String by System.getProperties()
+    kotlin("jvm").version(kotlinVersion)
+}
+base {
+    val archivesBaseName: String by project
+    archivesName.set(archivesBaseName)
+}
+val modVersion: String by project
+version = modVersion
+val mavenGroup: String by project
+group = mavenGroup
+minecraft {}
+repositories {}
+dependencies {
+    val minecraft_version: String by project
+    minecraft("com.mojang:minecraft:$minecraft_version")
+    val yarn_mappings: String by project
+    mappings("net.fabricmc:yarn:$yarn_mappings:v2")
+    val loader_version: String by project
+    modImplementation("net.fabricmc:fabric-loader:$loader_version")
+    val fabric_version: String by project
+    modImplementation("net.fabricmc.fabric-api:fabric-api:$fabric_version")
+    val fabricKotlinVersion: String by project
+    modImplementation("net.fabricmc:fabric-language-kotlin:$fabricKotlinVersion")
+}
+tasks {
+    val javaVersion = JavaVersion.VERSION_16
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        sourceCompatibility = javaVersion.toString()
+        targetCompatibility = javaVersion.toString()
+        options.release.set(javaVersion.toString().toInt())
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions { jvmTarget = javaVersion.toString() }
+        sourceCompatibility = javaVersion.toString()
+        targetCompatibility = javaVersion.toString()
+    }
+    jar { from("LICENSE") { rename { "${it}_${base.archivesName}" } } }
+    processResources {
+        inputs.property("version", project.version)
+        filesMatching("fabric.mod.json") { expand(mutableMapOf("version" to project.version)) }
+    }
+    java {
+        toolchain { languageVersion.set(JavaLanguageVersion.of(javaVersion.toString())) }
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
+        withSourcesJar()
+    }
+}
---
+++ src/main/kotlin/codes/edaz/op_gear/OPGear.kt
@@ -0,0 +1,9 @@
+package codes.edaz.op_gear
+import net.fabricmc.api.ModInitializer
+@Suppress("UNUSED")
+object OPGear: ModInitializer {
+    private const val MOD_ID = "mod_id"
+    override fun onInitialize() {
+        println("Example mod has been initialized.")
+    }
+}
